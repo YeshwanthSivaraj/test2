d2bad91ec372da5f171637808c561433aacc4725
YeshwanthSivaraj <mail2yeshwanthsivaraj@gmail.com>
2021-08-23 00:10:47 +0530

    c4

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/', (req, res) => {
 
         if (payload.Type === 'SubscriptionConfirmation') {
             try{
-                const data = fetch(payload.ubscribeURL)
+                const data = fetch(payload.SubscribeURL)
                 console.log('Subscription confirmed')
                 res.end("ok")
             } catch (err) {
