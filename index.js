6fba4becb47d7bc38b06e883280ba28e0abaf3cf
YeshwanthSivaraj <mail2yeshwanthsivaraj@gmail.com>
2021-11-24 12:55:50 +0530

    url test

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("Listening on port:", port)
-})
+  console.log("Listening on port:", port);
+});
 
-app.get('/', (req, res) => {
-    res.send('SES SNS-falls')
-})
+app.get("/", (req, res) => {
+  res.send("SES SNS-falls");
+});
 
-app.post('/', (req, res) => {
-    let body = ''
-    req.on('data', (chunk) => {
-        body += chunk.toString()
-    })
+app.post("/", (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
 
-    req.on('end', () => {
-        let payload = JSON.parse(body)
+  req.on("end", () => {
+    let payload = JSON.parse(body);
+    console.log(payload);
 
-        let message = JSON.parse(payload.Message)
+    // let message = JSON.parse(payload.Message)
 
-        const status = message.eventType
-        const messageID = message.mail.messageId
-        const timestamp = new Date(message.mail.timestamp)
-        const email_id = message.mail.destination[0]
-        const subject = message.mail.commonHeaders.subject
-        let description = ''
+    // const status = message.eventType
+    // const messageID = message.mail.messageId
+    // const timestamp = new Date(message.mail.timestamp)
+    // const email_id = message.mail.destination[0]
+    // const subject = message.mail.commonHeaders.subject
+    // let description = ''
 
-        if (message.eventType === "Bounce") {
-            description = message.bounce.bouncedRecipients.diagnosticCode
-        } else if (message.eventType === "Complaint") {
-            description = message.complaint.complaintFeedbackType
-        } else if (message.eventType === "Delivery") {
-            description = message.delivery.smtpResponse
-        } else if (message.eventType === "Reject") {
-            description = message.reject.reason
-        }
+    // if (message.eventType === "Bounce") {
+    //     description = message.bounce.bouncedRecipients.diagnosticCode
+    // } else if (message.eventType === "Complaint") {
+    //     description = message.complaint.complaintFeedbackType
+    // } else if (message.eventType === "Delivery") {
+    //     description = message.delivery.smtpResponse
+    // } else if (message.eventType === "Reject") {
+    //     description = message.reject.reason
+    // }
 
-        const result = {
-            status,
-            messageID,
-            timestamp,
-            email_id,
-            subject,
-            description
-        }
+    // const result = {
+    //     status,
+    //     messageID,
+    //     timestamp,
+    //     email_id,
+    //     subject,
+    //     description
+    // }
 
-        console.log(result)
+    // console.log(result)
 
-        res.end('ok')        
-    })
-})
+    res.end("ok");
+  });
+});
